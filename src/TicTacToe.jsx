d4b1ff34a2a20ac112ac46a42489d8c4911355e1
Dani <danichilling@gmail.com>
2024-04-26 12:22:05 +0100

    gameState functionality added in, determines whether there is a winner, it's a draw or to play on

---
+++ src/TicTacToe.jsx
@@ -0,0 +1,108 @@
+// @ts-check
+import { useState } from "react";
+import "./index.css";
+import React from "react";
+import checkWinner from "./WinningPatterns";
+
+export default function TicTacToe() {
+  /**
+   * @typedef {null | 'x' | 'o'} CellState
+   * */
+  /**
+   * @type {CellState[]}
+   */
+
+  const initialBoardState = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ];
+  const [boardState, setBoardState] = useState(initialBoardState);
+
+  function whoseTurnIsIt() {
+    const numXs = boardState.filter((element) => element === "x").length;
+    const numOs = boardState.filter((element) => element === "o").length;
+
+    if (numXs >= numOs) {
+      return "o";
+    } else {
+      return "x";
+    }
+  }
+
+  /**
+   * @param {CellState[]} inputArray
+   * @returns
+   */
+
+  function createGridCellDivs(inputArray) {
+    let arrayOfDivs = inputArray.map((cellState, index) => {
+      // onClick console.logs the index of the element that is clicked
+
+      if (cellState === "x") {
+        return (
+          <div
+            className="cell"
+            key={index}
+            onClick={() => handleClickCell(index)}
+          >
+            ✖️
+          </div>
+        ); // 'x'
+      } else if (cellState === "o") {
+        return (
+          <div
+            className="cell"
+            key={index}
+            onClick={() => handleClickCell(index)}
+          >
+            ⭕
+          </div>
+        ); // 'o'
+      } else {
+        return (
+          <div
+            className="cell"
+            key={index}
+            onClick={() => handleClickCell(index)}
+          ></div>
+        ); //null
+      }
+    });
+    return arrayOfDivs;
+  }
+
+  function handleClickCell(index) {
+    const copyOfBoardState = [...boardState]; // shallow copy the current array of the board state
+
+    if (copyOfBoardState[index] === null) {
+      copyOfBoardState[index] = whoseTurnIsIt();
+      setBoardState(copyOfBoardState); // update state with copy of array if the cell is null
+    } else {
+      console.log(index, " is not null");
+    }
+  }
+
+  function resetBoardState() {
+    console.log("this is a reset button");
+    setBoardState(initialBoardState);
+  }
+
+  return (
+    <div className="gameContainer">
+      <div className="gameState">
+        {boardState.includes(null) ? checkWinner(boardState) : "its a draw"}
+      </div>
+      <div className="gameGrid">{createGridCellDivs(boardState)}</div>
+      <div className="resetButton">
+        <button onClick={resetBoardState}>reset</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/WinningPatterns.jsx
@@ -0,0 +1,21 @@
+const winningPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8], // Rows
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8], // Columns
+  [0, 4, 8],
+  [2, 4, 6], // Diagonals
+];
+
+export default function checkWinner(board) {
+  for (let i = 0; i < winningPatterns.length; i++) {
+    const [a, b, c] = winningPatterns[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return <div>{board[a]} is the winner!</div>;
+    }
+  }
+
+  return <div>play on!</div>;
+}
